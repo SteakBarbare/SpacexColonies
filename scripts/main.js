581c44d993c13fc3ec5f92819eed56bfb867181f
Lucas Lareginie <lareginielucas@hotmail.fr>
2017-12-18 14:30:01 +0100

    Added style :art:

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 //Create a Pixi Application
-let app = new PIXI.Application({width: 3000, height: 3000});
+let app = new PIXI.Application({
+    width: 3000,
+    height: 3000,
+
+
+});
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
