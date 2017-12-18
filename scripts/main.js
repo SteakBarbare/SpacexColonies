598eb8f9258703e47b04d255bd4725fc9b2e5519
SteakBarbare <corto.dufour-fakhouri@hetic.net>
2017-12-18 14:20:09 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+//Create a Pixi Application
+let app = new PIXI.Application({width: 3000, height: 3000});
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
