c3716dc060072dd5e45f2d5b7ca614d4f72b7ff2
Lucas Lareginie <lareginielucas@hotmail.fr>
2017-12-18 16:14:38 +0100

    :wrench: Restructured the menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 
 //SETUP
-
 let Application = PIXI.Application,
 loader = PIXI.loader,
 resources = PIXI.loader.resources,
@@ -19,91 +18,101 @@ document.body.appendChild(app.view)
 const canvas =  document.querySelector("canvas")
 const context = canvas.getContext("2d")
 
-const starSet = () => {
-    //CREATE PARTICLES
-    const particles = []
-    const create = () => {
-        for(i=0;i<800;i++){
-            const particle ={}
-            particle.x= (Math.random() * canvas.width)    
-            particle.y= (Math.random() * canvas.height)   
-            particles.push(particle)
-            particle.color='rgba(255, 255,255, 1)'
-            particle.luminosity="1"
-            particle.radius=Math.random() * 2
-        }
-        
-    
-    }
-    //UPDATING THEM
-    const update = () => {
-        for (const particle of particles){
-            //radius
-            let radiusChanges = Math.random() * 0.05
-            let radiusSign = Math.round(Math.random())
-            if(radiusSign == 1){
-                if((particle.radius + radiusChanges) < 3){
-                    particle.radius += radiusChanges
-                }
-            }
-            else if (radiusSign == 0) {
-                if ( (Math.round(particle.radius)) - radiusChanges > 0.001){
-                    particle.radius -= radiusChanges
-                }
-            }
-    
-            //Color effect
-            let currentLumi=particle.luminosity
-            let lumiSign=Math.round(Math.random())
-            let lumiChange= Math.random() * 0.2
-            if(lumiSign == 1){
-                if((particle.luminosity + lumiChange) < 1){
-                    particle.color = `rgba(255, 255,255, ${currentLumi += lumiChange})`
-                }
-            }
-            else if (lumiSign == 0) {
-                if ( (Math.round(particle.luminosity)) - lumiChange > 0.001){
-                    particle.color = `rgba(255, 255,255, ${currentLumi -= lumiChange})`
-                }
+
+const menuLaunch = () => {
+    const starSet = () => {
+        //CREATE PARTICLES
+        const particles = []
+        const create = () => {
+            for(i=0;i<800;i++){
+                const particle ={}
+                particle.x= (Math.random() * canvas.width)    
+                particle.y= (Math.random() * canvas.height)   
+                particles.push(particle)
+                particle.color='rgba(255, 255,255, 1)'
+                particle.luminosity="1"
+                particle.radius=Math.random() * 2
             }
             
-            //Deleting
-            if (particle.x < 0 || particle.x > canvas.width || particle.y < 0 || particle.y > canvas.height)
-            {
-            particles.splice(i,1)   
-            }
-        }   
-    }
-
-    //DRAWING THEM
-    const draw = () => 
-    {
-        context.globalCompositeOperation="source-over"
-        for(const particle of particles)
-        {
-            context.beginPath()
-            context.arc(particle.x,particle.y,particle.radius,0,Math.PI * 2)
-            context.fillStyle=particle.color
-            context.fill()
+        
         }
+        //UPDATING THEM
+        const update = () => {
+            for (const particle of particles){
+                //radius
+                let radiusChanges = Math.random() * 0.05
+                let radiusSign = Math.round(Math.random())
+                if(radiusSign == 1){
+                    if((particle.radius + radiusChanges) < 3){
+                        particle.radius += radiusChanges
+                    }
+                }
+                else if (radiusSign == 0) {
+                    if ( (Math.round(particle.radius)) - radiusChanges > 0.001){
+                        particle.radius -= radiusChanges
+                    }
+                }
+        
+                //Color effect
+                let currentLumi=particle.luminosity
+                let lumiSign=Math.round(Math.random())
+                let lumiChange= Math.random() * 0.2
+                if(lumiSign == 1){
+                    if((particle.luminosity + lumiChange) < 1){
+                        particle.color = `rgba(255, 255,255, ${currentLumi += lumiChange})`
+                    }
+                }
+                else if (lumiSign == 0) {
+                    if ( (Math.round(particle.luminosity)) - lumiChange > 0.001){
+                        particle.color = `rgba(255, 255,255, ${currentLumi -= lumiChange})`
+                    }
+                }
+                
+                //Deleting
+                if (particle.x < 0 || particle.x > canvas.width || particle.y < 0 || particle.y > canvas.height)
+                {
+                particles.splice(i,1)   
+                }
+            }   
+        }
+    
+        //DRAWING THEM
+        const draw = () => 
+        {
+            context.globalCompositeOperation="source-over"
+            for(const particle of particles)
+            {
+                context.beginPath()
+                context.arc(particle.x,particle.y,particle.radius,0,Math.PI * 2)
+                context.fillStyle=particle.color
+                context.fill()
+            }
+            
+        }
+        //CLEARING THEM
+        const clear = () => {
+            context.clearRect(0,0,canvas.width,canvas.height)
+        }
+        create()
+    
+        const loop = () =>{
+            window.requestAnimationFrame(loop)
+            
+            update()
+            clear()
+            draw()
+            
+        }
+        loop()
+    
+    }
+    starSet()
+    const menuSet = () => {
         
     }
-    //CLEARING THEM
-    const clear = () => {
-        context.clearRect(0,0,canvas.width,canvas.height)
-    }
-    create()
 
-    const loop = () =>{
-        window.requestAnimationFrame(loop)
-        
-        update()
-        clear()
-        draw()
-        
-    }
-    loop()
 
-}
-starSet()
+} 
+
+
 
