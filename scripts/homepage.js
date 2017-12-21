0f66320c2351633f7fdab4c56474d602ac06b21c
guillaumeRech <34440383+guillaumeRech@users.noreply.github.com>
2017-12-21 18:41:47 +0100

    Add files via upload

---
+++ scripts/homepage.js
@@ -0,0 +1,128 @@
+const homePage = () => {
+//SETUP
+let Application = PIXI.Application,
+loader = PIXI.loader,
+resources = PIXI.loader.resources,
+Sprite = PIXI.Sprite
+//Create a Pixi Application
+let app = new Application({ 
+    width: 3000, 
+    height: 3000,                       
+    antialias: true, 
+    transparent: false, 
+    resolution: 1,
+    forceCanvas: true
+})
+app.renderer.autoResize = true;
+document.body.appendChild(app.view)
+
+const canvas =  document.querySelector("canvas")
+const tiltCanv = (elem) => {
+    const presp="500px",
+    width=elem.offsetWidth,
+    height=elem.offsetHeight,
+    midWidth=width / 2,
+    midHeight=height / 2
+
+    document.addEventListener("mousemove",(event) => {
+        const posTop = elem.offsetTop,
+        posLeft = elem.offsetLeft,
+        perspective="100px",
+        cursorX = event.offsetX - posLeft,
+        cursorY = event.offsetY - posTop,
+        cursorCenterX = midWidth - cursorX,
+        cursorCenterY = midHeight - cursorY,
+        angle=150
+        elem.style.transform='perspective(' + perspective + ') rotateX('+(cursorCenterY / angle) +'deg) rotateY('+ -(cursorCenterX / angle) + 'deg)';
+        elem.classList.remove("isOut") 
+        elem.addEventListener("mouseover",(event) => {
+            
+            
+        })
+    })
+    document.addEventListener("mouseout",(event) => {
+        elem.classList.add("isOut")
+    })
+}
+const loop = () => {
+    window.requestAnimationFrame(loop)
+    // tiltCanv(document.querySelector(".selecPlay"))
+}
+loop()
+const menuLaunch = () => {
+    const starSet = () => {
+        const starArray = []
+        //CREATING THE STARS PARTICLES
+        for(i=0;i<1000;i++){    
+            let x= (Math.random() * document.body.offsetWidth)    
+            let y= (Math.random() * document.body.offsetHeight)   
+            let radius=Math.random() * 1.5  
+            let star = new PIXI.Graphics()
+            starArray[i]=star
+            star.beginFill(0xFFFFFF)
+            star.drawCircle(x,y,radius)
+            app.stage.addChild(star);
+            
+        }  
+    }   
+    starSet()
+    const earthArea = document.querySelector(".earthArea")
+    const menuSet = () => {  
+        //VARIABLES
+        let menuState = 0
+        const actionB = document.querySelector(".selecPlay")
+        const logoTitle =document.querySelector(".logoTitle")
+        const goalArea = document.querySelector(".goalArea")
+        const bottomLink= document.querySelector(".bottomLink")
+        const interactiveMenu=document.querySelector(".interactiveMenu")
+        const hyperSpace= document.querySelector(".hyperSpace")
+        const marsPlanet = document.querySelector(".marsPlanet")
+                
+        actionB.addEventListener("click", (event) => {
+            
+            if (menuState == 0){
+                const hyperSpaceStage = () => {
+                    hyperSpace.style.display="inline-block"
+                    canvas.style.opacity=0
+                    interactiveMenu.style.opacity=0
+                    actionB.style.opacity=0
+                    document.body.style.background="black"
+                    hyperSpace.style.transform="scale(0.5)"
+                    setTimeout(function(){
+                        canvas.style.opacity=1
+                        interactiveMenu.style.opacity=1
+                        actionB.style.opacity=1
+                        hyperSpace.style.display="none"
+                        midMenuStage()
+                    },4400)
+
+                }
+                hyperSpaceStage()
+
+                const midMenuStage = () => {
+                    //MARS ARRIVAL
+                    actionB.innerHTML="P L A Y"
+                    logoTitle.style.transform="translateX(-400px)"
+                    goalArea.style.opacity=1
+                    bottomLink.style.opacity=1
+                    marsPlanet.style.opacity=1
+                    marsPlanet.style.animation="planetArrival 0.5s cubic-bezier(.24,.4,.87,1.08)"
+                    actionB.style.top="80%"
+                    menuState=1
+                    console.log(menuState)
+                    }
+                }
+                
+            else if (menuState == 1){
+                document.location.href="pages/game.html"; 
+            }
+        })
+        
+    }
+
+    menuSet()
+
+} 
+menuLaunch()
+
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,420 +1,15 @@
-let buildingName = "headQuarters";
-
-let Application = PIXI.Application,
-    loader = PIXI.loader,
-    resources = PIXI.loader.resources,
-    Sprite = PIXI.Sprite;
-
-//Create a Pixi Application
-let app = new PIXI.Application({
-    width: 1920,
-    height: 1080,
-    antialias: true, 
-    transparent: false, 
-    resolution: 1,
-    forceCanvas: true
-});
-
-//Add the canvas that Pixi automatically created for you to the HTML document
-document.body.appendChild(app.view);
-const canvasBuilder = document.querySelector('canvas');
+//AUDIO RELATED VARIABLES
 
 
-app.renderer.backgroundColor = 0xc1440e;
-app.renderer.view.style.position = "absolute";
-app.renderer.view.style.display = "block";
-app.renderer.autoResize = true;
-app.renderer.resize(window.innerWidth, window.innerHeight)
 
-PIXI.loader
-.add("map", "images/map.png")
-.add("headQuarters", "images/headquarters.png")
-.add("D3Printer", "images/3DPrinter.png")
-.add("solarTurbine", "images/solarTurbine.png")
-.add("greenhouse", "images/greenhouse.png")
-.add("silo", "images/silo.png")
-.add("battery", "images/battery.png")
-.add("houses", "images/houses.png")
-.add("drill", "images/drill.png")
-.add("sickBay", "images/sickBay.png")
-.add("launchingRamp", "images/launchingRamp.png")
-.add("leindenfrostTurbine", "images/leindenfrostTurbine.png")
-.add("sportsHall", "images/sportsHall.png")
-.add("sulfurFactory", "images/sulfurFactory.png")
-.add("warehouse", "images/warehouse.png")
-.add("hydroOxygenComplex", "images/hydroOxygenComplex.png")
-
-.load(setupIni);
-
-// Sprite Création
-
-let sprite;
-
-function setup() {
-    const coordX = sprite.x;
-    const coordY = sprite.y;
-    console.log("Pute");
-    if(buildingName == "headQuarters"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.headQuarters.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "greenhouse"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.greenhouse.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "D3Printer"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.D3Printer.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "solarTurbine"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.solarTurbine.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "silo"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.silo.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "battery"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.battery.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "houses"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.houses.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "drill"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.drill.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "sickBay"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.sickBay.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "launchingRamp"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.launchingRamp.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "leindenfrostTurbine"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.leindenfrostTurbine.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "sportsHall"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.sportsHall.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "sulfurFactory"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.sulfurFactory.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "warehouse"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.warehouse.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }else if(buildingName == "hydroOxygenComplex"){
-        sprite = new PIXI.Sprite(PIXI.loader.resources.hydroOxygenComplex.texture);
-        sprite.x = coordX;
-        sprite.y = coordY;
-        app.stage.addChild(sprite);
-    }
-}
-
-function setupIni() {
-
-    const coordX = (canvasBuilder.width / 2 - (canvasBuilder.width / 2 % 100)) / 100;
-    const coordY = (canvasBuilder.height / 2 - (canvasBuilder.height / 2 % 100)) / 100;
-    console.log("Jeej");
-    sprite = new PIXI.Sprite(PIXI.loader.resources.map.texture);
-    app.stage.addChild(sprite);
-    sprite = new PIXI.Sprite(PIXI.loader.resources.headQuarters.texture);
-    sprite.x = coordX * 100;
-    sprite.y = coordY * 100;
-    grid[coordX][coordY][2] = "headQuarters";
-    app.stage.addChild(sprite);
-    sprite = new PIXI.Sprite(PIXI.loader.resources.headQuarters.texture);
-    app.stage.removeChild(sprite);
-    selection = true;
-}
-
-// Ressources
-
-let energy = 150,
-    maxEnergy = 150,
-    materials = 100,
-    maxMaterials = 100,
-    pilgrims = 20,
-    maxPilgrims = 20,
-    food = 100,
-    maxFood = 100;
-
-// Grid Ini
-
-let grid = new Array(30);
-let selection = true;
-
-for (let i = 0; i < grid.length; i++){
-    grid[i] = [i*100];
-    for(let j = 0; j < grid.length; j++){
-        grid[i][j] = [i*100, j*100, 0];
-    }
-}
-
-// Construction Previz
-
-canvasBuilder.addEventListener(
-    'mousemove',
-    (e) =>{
-        sprite.x = e.clientX - (e.clientX % 100);
-        sprite.y = e.clientY - (e.clientY % 100);
-    }
-)
-
-// Building Placement
-
-canvasBuilder.addEventListener(
-    'click',
-    (e) =>{
-        const coordX = ((e.clientX - (e.clientX % 100)) / 100);
-        const coordY = ((e.clientY - (e.clientY % 100)) / 100);
-        let conditions = true;
-        if((grid[coordX][coordY][2] == 0) && (selection == false)){
-            for(let i = 1; i < buildings.length; i++){
-                if(buildings[i].gameName == buildingName){
-                    for(let j = 0; j < buildings[i].condition.length; j++){
-                        let bool = false;
-                        for(let fuuf = 0; fuuf < grid.length; fuuf++){
-                            for(let jeej = 0; jeej < grid.length; jeej++){
-                                if(grid[fuuf][jeej][2] == buildings[i].condition[j]){
-                                    bool = true;
-                                }
-                            }
-                        }
-                        if(bool == false){
-                            conditions = false;
-                        }
-                    }
-                    if(conditions == true){
-                        if((materials >= buildings[i].materialsPrice) && (energy >= buildings[i].energyPrice)){
-                            materials -= buildings[i].materialsPrice;
-                            energy -= buildings[i].energyPrice
-                            setup();
-                            grid[coordX][coordY][2] = buildingName;
-                        }
-                    }else{
-                        window.alert(`Ce bâtiment demande au préalable la construction de: ${buildings[i].condition}`);
-                    }
-                }
-            }     
-        }else{
-            app.stage.removeChild(sprite);
-            console.log(grid[coordX][coordY][2]);
-            selection = true;
-        }
-    }
-)
-
-// Selection mode
-
-document.addEventListener(
-    'keydown',
-    (e) =>{
-        if(e.keyCode == 27){
-            app.stage.removeChild(sprite);
-            selection = true;
-        }
-    }
-)
-
-// Ressources Depletion
-
-const actualFood = document.querySelector('.food');
-const actualPilgrims = document.querySelector('.pilgrims');
-
-// Food Consumption & Production
-
-setInterval(
-    ()=>{
-        let production = 0;
-        for(let i = 0; i < grid.length; i++){
-            for(let j = 0; j < grid.length; j++){
-                if(grid[i][j][2] == "greenhouse"){
-                    production++;
-                }
-            }
-        }
-        food -= 0.05 * pilgrims - (production * 0.6);
-// Ressources Consumption & Production
-
-setInterval(
-    ()=>{
-        //  Buildings
-        let greenH = 0;
-            hydro = 0,
-            printer = 0,
-            generator = 0,
-            factory = 0,
-            energyStock = 0,
-            wareH = 0,
-            habitations = 0,
-            sickB = 0,
-            sport = 0,
-            leiden = 0,
-            fuelDrill = 0,
-            launcher = 0;
-
-        //  Food Production/Depletion
-
-        //  Searching through the grid for food buildings
-        for(let i = 0; i < grid.length; i++){
-            for(let j = 0; j < grid.length; j++){
-                if(grid[i][j][2] == "greenhouse"){
-                    greenH++;
-                }else if(grid[i][j][2] == "hydroOxygenComplex"){
-                    hydro++
-                }
-            }
-        }
-
-        // Food update 
-        food -= 0.05 * pilgrims - (greenH * 0.6) - (hydro * 0.1);
-        if(food < 0){
-            food = 0;
-        }else if(food > maxFood){
-            food = maxFood;
-        }
+homePage()
 
 
-        //  Energy Production/Depletion
-
-        greenH = 0;
-        hydro = 0;
-
-        //  Searching through the grid for energy buildings
-        for(let i = 0; i < grid.length; i++){
-            for(let j = 0; j < grid.length; j++){
-                if(grid[i][j][2] == "greenhouse"){
-                    greenH++;
-                }else if(grid[i][j][2] == "hydroOxygenComplex"){
-                    hydro++
-                }else if(grid[i][j][2] == "D3Printer"){
-                    printer++
-                }else if(grid[i][j][2] == "solarTurbine"){
-                    generator++
-                }else if(grid[i][j][2] == "sulfurFactory"){
-                    factory++
-                }else if(grid[i][j][2] == "battery"){
-                    energyStock++
-                }else if(grid[i][j][2] == "warehouse"){
-                    wareH++
-                }else if(grid[i][j][2] == "houses"){
-                    habitations++
-                }else if(grid[i][j][2] == "sickBay"){
-                    sickB++
-                }else if(grid[i][j][2] == "sportsHall"){
-                    sport++
-                }else if(grid[i][j][2] == "leindenfrostTurbine"){
-                    leiden++
-                }else if(grid[i][j][2] == "drill"){
-                    fuelDrill++
-                }else if(grid[i][j][2] == "launchingRamp"){
-                    launcher++
-                }
-            }
-        }
-
-        // Energy update 
-        energy -= -5 + (printer * 5) - (generator * 30) + (hydro * 5) + (greenH * 5) + (factory * 5) + (energyStock *5 ) + (wareH * 5) + (habitations * 8) + (sickB * 15) + (sport * 20) - (leiden * 200) + (fuelDrill * 100) + (launcher * 50);
-        
-        if(energy < 0){
-            energy = 0;
-        }else if(energy > maxEnergy){
-            energy = maxEnergy;
-        }
-        
-        //  Materials Production/Depletion
-
-        factory = 0;
-
-        //  Searching through the grid for energy buildings
-        for(let i = 0; i < grid.length; i++){
-            for(let j = 0; j < grid.length; j++){
-                if(grid[i][j][2] == "sulfurFactory"){
-                    factory++
-                }
-            }
-        }
-
-        // Energy update 
-        materials += 0.5 + (factory * 1);
-        
-        if(materials < 0){
-            materials = 0;
-        }else if(materials > maxMaterials){
-            materials = maxMaterials;
-        }
-    },
-    1000
-);
-
-//  Death by Starvation
-
-setInterval(
-    ()=>{
-        if(food <= 0){
-            pilgrims -= 1;
-        }
-    },
-    5000
-);
-
-// Notifications
-setInterval(
-    ()=>{
-        console.log(`Food Left: ${Math.trunc(food)}`); 
-        console.log(`Energy left: ${energy}`);
-        console.log(`Materials: ${materials}`);
-        console.log(`Pilgrims Alive: ${pilgrims}`);
-    },
-    100
-);
-
-// Building Selection from DOM
-
-const buildingButtons = document.querySelectorAll('.buildingsButtons');
-
-for(let i = 0; i < buildingButtons.length; i++){
-    buildingButtons[i].addEventListener(
-        'click',
-        (event)=> {
-            buildingName = buildings[i + 1].gameName;
-            console.log(buildingName);
-            app.stage.removeChild(sprite);
-            setup();
-            selection = false;
-        }
-    )
-}
-
-//Buildings objects stats
+//Buildings cost and effects
 
 const headQuarters = {
-    name: "HeadQuarter",
-    gameName: "headQuarters",
+    name: "HeadQuarters",
     description: "",
-    condition: [],
     materialsPrice: 0,
     materialsProduction: 0,
     materialLimit: 0,
@@ -428,14 +23,12 @@ const headQuarters = {
 }
 
 const threeDPrinter = {
-    name: "3D Printer",
-    gameName: "D3Printer",
+    name: "3D Pinter",
     description: "",
-    condition: ["headQuarters"],
-    materialsPrice: 10,
+    materialsPrice: 0,
     materialsProduction: 0,
     materialLimit: 0,
-    energyPrice: 10,
+    energyPrice: 0,
     energyProduction: 0,
     energyUsed: 5,
     energyLimit: 0,
@@ -446,9 +39,7 @@ const threeDPrinter = {
 
 const hydroOxygenComplex = {
     name: "Hydro-Oxygen Complex",
-    gameName: "hydroOxygenComplex",
     description: "",
-    condition: ["headQuarters", "D3Printer"],
     materialsPrice: 25,
     materialsProduction: 0,
     materialLimit: 0,
@@ -463,9 +54,7 @@ const hydroOxygenComplex = {
 
 const solarTurbine = {
     name: "Solar Turbine",
-    gameName: "solarTurbine",
     description: "",
-    condition: ["headQuarters", "D3Printer"],
     materialsPrice: 25,
     materialsProduction: 0,
     materialLimit: 0,
@@ -480,13 +69,11 @@ const solarTurbine = {
 
 const greenhouse = {
     name: "Greenhouse",
-    gameName: "greenhouse",
     description: "",
-    condition: ["headQuarters", "D3Printer"],
     materialsPrice: 20,
     materialsProduction: 0,
     materialLimit: 0,
-    energyPrice: 20,
+    energyPrice: 100,
     energyProduction: 0,
     energyUsed: 10,
     energyLimit: 0,
@@ -497,9 +84,7 @@ const greenhouse = {
 
 const sulfurFactory = {
     name: "Sulfur Factory",
-    gameName: "sulfurFactory",
     description: "",
-    condition: ["headQuarters", "D3Printer", "solarTurbine", "greenhouse"],
     materialsPrice: 25,
     materialsProduction: 5,
     materialLimit: 0,
@@ -514,9 +99,7 @@ const sulfurFactory = {
 
 const silo = {
     name: "Silo",
-    gameName: "silo",
     description: "",
-    condition: ["headQuarters", "D3Printer", "greenhouse"],
     materialsPrice: 50,
     materialsProduction: 0,
     materialLimit: 0,
@@ -531,9 +114,7 @@ const silo = {
 
 const battery = {
     name: "Battery",
-    gameName: "battery",
     description: "",
-    condition: ["headQuarters", "D3Printer", "solarTurbine"],
     materialsPrice: 200,
     materialsProduction: 0,
     materialLimit: 0,
@@ -548,9 +129,7 @@ const battery = {
 
 const warehouse = {
     name: "Warehouse",
-    gameName: "warehouse",
     description: "",
-    condition: ["headQuarters", "D3Printer", "sulfurFactory"],
     materialsPrice: 150,
     materialsProduction: 0,
     materialLimit: 100,
@@ -565,9 +144,7 @@ const warehouse = {
 
 const launchingRamp = {
     name: "Launching ramp",
-    gameName: "launchingRamp",
     description: "",
-    condition: ["headQuarters", "D3Printer", "drill"],
     materialsPrice: 150,
     materialsProduction: 0,
     materialLimit: 0,
@@ -582,9 +159,7 @@ const launchingRamp = {
 
 const houses = {
     name: "Houses",
-    gameName: "houses",
     description: "",
-    condition: ["headQuarters", "D3Printer", "sulfurFactory"],
     materialsPrice: 35,
     materialsProduction: 0,
     materialLimit: 0,
@@ -599,9 +174,7 @@ const houses = {
 
 const sickBay = {
     name: "Sick Bay",
-    gameName: "sickBay",
     description: "",
-    condition: ["headQuarters", "D3Printer", "sulfurFactory", "houses"],
     materialsPrice: 40,
     materialsProduction: 0,
     materialLimit: 0,
@@ -616,9 +189,7 @@ const sickBay = {
 
 const sportsHall = {
     name: "Sports Hall",
-    gameName: "sportsHall",
     description: "",
-    condition: ["headQuarters", "D3Printer", "houses"],
     materialsPrice: 100,
     materialsProduction: 0,
     materialLimit: 0,
@@ -633,9 +204,7 @@ const sportsHall = {
 
 const leindenfrostTurbine = {
     name: "Leindenfrost Turbine",
-    gameName: "leindenfrostTurbine",
     description: "",
-    condition: ["headQuarters", "D3Printer", "solarTurbine"],
     materialsPrice: 300,
     materialsProduction: 0,
     materialLimit: 0,
@@ -650,9 +219,7 @@ const leindenfrostTurbine = {
 
 const oilSlickDrill = {
     name: "Oil Slick Drill",
-    gameName: "drill",
     description: "",
-    condition: ["headQuarters", "D3Printer", "houses", "sickBay", "sportsHall"],
     materialsPrice: 800,
     materialsProduction: 0,
     materialLimit: 0,
@@ -666,21 +233,7 @@ const oilSlickDrill = {
 }
 
 //Array of all the buldings objects
+const buildingsDetails =[headQuarters, threeDPrinter, hydroOxygenComplex, solarTurbine, greenhouse, sulfurFactory, silo, battery, warehouse, launchingRamp, houses, sickBay, sportsHall, leindenfrostTurbine, oilSlickDrill]
 
-const buildings =[
-    headQuarters, 
-    threeDPrinter, 
-    hydroOxygenComplex, 
-    solarTurbine, 
-    greenhouse, 
-    sulfurFactory, 
-    silo, 
-    battery, 
-    warehouse, 
-    launchingRamp,
-    houses, 
-    sickBay, 
-    sportsHall, 
-    leindenfrostTurbine, 
-    oilSlickDrill
-];
+console.log(buildingsDetails)
+
