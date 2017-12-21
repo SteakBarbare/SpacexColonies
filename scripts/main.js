41eb33f4c5de2e8911115dfaf62271ec565a4b90
Lucas Lareginie <lareginielucas@hotmail.fr>
2017-12-21 15:21:30 +0100

    Merged everything

---
+++ scripts/main.js
@@ -0,0 +1,235 @@
+
+homePage()
+
+//Buildings cost and effects
+
+const headQuarters = {
+    name: "HeadQuarters",
+    description: "",
+    materialsPrice: 0,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 0,
+    energyProduction: 0,
+    energyUsed: 0,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+const threeDPrinter = {
+    name: "3D Pinter",
+    description: "",
+    materialsPrice: 0,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 0,
+    energyProduction: 0,
+    energyUsed: 5,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+const hydroOxygenComplex = {
+    name: "Hydro-Oxygen Complex",
+    description: "",
+    materialsPrice: 25,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 50,
+    energyProduction: 0,
+    energyUsed: 5,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+const solarTurbine = {
+    name: "Solar Turbine",
+    description: "",
+    materialsPrice: 25,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 20,
+    energyProduction: 20,
+    energyUsed: 5,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+const greenhouse = {
+    name: "Greenhouse",
+    description: "",
+    materialsPrice: 20,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 100,
+    energyProduction: 0,
+    energyUsed: 10,
+    energyLimit: 0,
+    foodProduction: 4,
+    foodLimit: 0,
+    pop: 0
+}
+
+const sulfurFactory = {
+    name: "Sulfur Factory",
+    description: "",
+    materialsPrice: 25,
+    materialsProduction: 5,
+    materialLimit: 0,
+    energyPrice: 130,
+    energyProduction: 0,
+    energyUsed: 5,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+const silo = {
+    name: "Silo",
+    description: "",
+    materialsPrice: 50,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 50,
+    energyProduction: 0,
+    energyUsed: 10,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 100,
+    pop: 0
+}
+
+const battery = {
+    name: "Battery",
+    description: "",
+    materialsPrice: 200,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 200,
+    energyProduction: 0,
+    energyUsed: 10,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+const warehouse = {
+    name: "Warehouse",
+    description: "",
+    materialsPrice: 150,
+    materialsProduction: 0,
+    materialLimit: 100,
+    energyPrice: 150,
+    energyProduction: 0,
+    energyUsed: 10,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+const launchingRamp = {
+    name: "Launching ramp",
+    description: "",
+    materialsPrice: 150,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 90,
+    energyProduction: 0,
+    energyUsed: 50,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+const houses = {
+    name: "Houses",
+    description: "",
+    materialsPrice: 35,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 50,
+    energyProduction: 0,
+    energyUsed: 10,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+const sickBay = {
+    name: "Sick Bay",
+    description: "",
+    materialsPrice: 40,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 100,
+    energyProduction: 0,
+    energyUsed: 10,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+const sportsHall = {
+    name: "Sports Hall",
+    description: "",
+    materialsPrice: 100,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 150,
+    energyProduction: 0,
+    energyUsed: 20,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+const leindenfrostTurbine = {
+    name: "Leindenfrost Turbine",
+    description: "",
+    materialsPrice: 300,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 500,
+    energyProduction: 45,
+    energyUsed: 0,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+const oilSlickDrill = {
+    name: "Oil Slick Drill",
+    description: "",
+    materialsPrice: 800,
+    materialsProduction: 0,
+    materialLimit: 0,
+    energyPrice: 300,
+    energyProduction: 0,
+    energyUsed: 100,
+    energyLimit: 0,
+    foodProduction: 0,
+    foodLimit: 0,
+    pop: 0
+}
+
+//Array of all the buldings objects
+const buildingsDetails =[headQuarters, threeDPrinter, hydroOxygenComplex, solarTurbine, greenhouse, sulfurFactory, silo, battery, warehouse, launchingRamp, houses, sickBay, sportsHall, leindenfrostTurbine, oilSlickDrill]
+
+console.log(buildingsDetails)
+
